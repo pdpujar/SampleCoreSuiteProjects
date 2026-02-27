686066e212ae30ced74c1a9706c457041f2d01ab
Prakash Pujar <prakash@dynamicpdf.com>
2026-02-27 16:16:26 +0530

    Fisrt Document creation

--- SimpleApplicationCoreSuite/Program.cs
+++ SimpleApplicationCoreSuite/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ceTe.DynamicPDF;
+using ceTe.DynamicPDF.PageElements;
 
 namespace SimpleApplicationCoreSuite
 {
@@ -10,10 +12,14 @@ namespace SimpleApplicationCoreSuite
     {
         static void Main(string[] args)
         {
-            for(int i=0;i<10; i++)
-            {
-                Console.WriteLine("Number is::" + (i + 1));
-            }
+            Document document = new Document();
+            Page page = new Page();
+            document.Pages.Add(page);
+            TextArea area = new TextArea("This is to test", 100, 100, 200, 200);
+            page.Elements.Add(area);
+            string outputPdf = @"C:\Temp\MyDocument.pdf";
+            document.Draw(outputPdf);
+            System.Diagnostics.Process.Start(outputPdf);
         }
     }
 }
